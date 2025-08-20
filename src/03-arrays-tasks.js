1581d87050254143db6cee5cc1ae3af57c23cf34
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-20 23:14:03 +0500

    feat: 29 tests passing

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -90,8 +90,9 @@ function doubleArray(arr) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  return arr.filter((element) => element > 0);
+  // throw new Error('Not implemented');
 }
 
 /**
