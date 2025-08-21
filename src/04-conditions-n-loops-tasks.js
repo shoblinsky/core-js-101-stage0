ea21ebb9eb85ad03bd3259529913195d157fecf4
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-21 22:03:07 +0500

    feat: 35 tests passing

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -93,8 +93,9 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a < b + c && b < a + c && c < a + b) return true;
+  return false;
 }
 
 /**
